9cc251dc932ab102d7754b479982c6f1345b1ab6
Wusch <florian.otto@mindbird.de>
2018-02-27 16:09:30 +0100

    Small improvements

--- src/UiToTop.php
+++ src/UiToTop.php
@@ -10,16 +10,16 @@ class Module extends ContaoModule
 
     protected $strTemplate = 'mod_uitotop';
 
+
     /**
-     * @return string|void
+     * @return string
      */
     public function generate()
     {
         $GLOBALS['TL_JAVASCRIPT'][] = '/system/modules/uitotop/assets/js/jquery.ui.totop.js|static';
         $GLOBALS['TL_CSS'][] = '/system/modules/uitotop/assets/css/ui.totop.css|screen|static';
-        $objTemplate = new FrontendTemplate($this->strTemplate);
-        $objTemplate->text = $this->uitotop_text;
-        $GLOBALS['TL_BODY'][] = $objTemplate->parse();
+
+        return parent::generate();
     }
 
     /**
