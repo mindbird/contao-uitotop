22ff404c214116bf7b96d4480f48bc22dff50865
Wusch <florian.otto@mindbird.de>
2018-04-06 11:30:17 +0200

    Minimize js and css files

--- src/UiToTopModule.php
+++ src/UiToTopModule.php
@@ -14,16 +14,13 @@ class UiToTopModule extends \Contao\Module
      */
     public function generate()
     {
-        $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/uitotop/js/jquery.ui.totop.js';
-        $GLOBALS['TL_CSS'][] = 'bundles/uitotop/css/ui.totop.css|screen';
+        $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/uitotop/js/jquery.ui.totop.min.js|static';
+        $GLOBALS['TL_CSS'][] = 'bundles/uitotop/css/ui.totop.min.css|screen|static';
         $objTemplate = new FrontendTemplate($this->strTemplate);
         $objTemplate->text = $this->uitotop_text;
         $GLOBALS['TL_BODY'][] = $objTemplate->parse();
     }
 
-    /**
-     *
-     */
     public function compile()
     {
         
