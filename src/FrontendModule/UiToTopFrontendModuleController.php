a87d8a21c0ed2e89a6438418a5c070ef6503ebdf
Florian Otto <florian.otto@mindbird.de>
2023-07-07 10:52:52 +0200

    Update to contao 5

--- src/FrontendModule/UiToTopFrontendModuleController.php
+++ src/FrontendModule/UiToTopFrontendModuleController.php
@@ -2,9 +2,11 @@
 
 namespace Mindbird\ContaoUiToTop\FrontendModule;
 
+use Contao\BackendTemplate;
 use Contao\Controller;
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
+use Contao\CoreBundle\Routing\ScopeMatcher;
 use Contao\CoreBundle\Twig\FragmentTemplate;
 use Contao\FrontendTemplate;
 use Contao\ModuleModel;
@@ -12,33 +14,34 @@ use Contao\Template;
 use Mindbird\ContaoNodeOverlay\Controller\NodesOverlayContentElementController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Terminal42\NodeBundle\NodeManager;
 
 #[AsContentElement(UiToTopFrontendModuleController::TYPE, category: "miscellaneous")]
 class UiToTopFrontendModuleController extends AbstractFrontendModuleController
 {
     public const TYPE = 'uitotop';
 
-    /**
-     * @return string|void
-     */
-    public function generate()
-    {
-        $GLOBALS['TL_CSS'][] = 'bundles/uitotop/css/ui.totop.min.css|screen|static';
-        $GLOBALS['TL_BODY'][] = Template::generateScriptTag(
-            Controller::addAssetsUrlTo('bundles/uitotop/js/jquery.ui.totop.min.js'),
-            false,
-            true
-        );
-    }
-
-    public function compile()
+    public function __construct(private readonly ScopeMatcher $scopeMatcher)
     {
 
     }
 
     protected function getResponse(FragmentTemplate $template, ModuleModel $model, Request $request): Response
     {
+        if ($this->scopeMatcher->isBackendRequest($request)) {
+            $template = new BackendTemplate('be_wildcard');
+            $template->wildcard = '### ' . $GLOBALS['TL_LANG']['CTE'][self::TYPE] . ' ###';
+            return $template->getResponse();
+        }
+
         $template->text = $model->text;
+
+        $GLOBALS['TL_CSS'][] = 'bundles/uitotop/css/ui.totop.min.css|screen|static';
+        $GLOBALS['TL_BODY'][] = \Contao\Template::generateScriptTag(
+            Controller::addAssetsUrlTo('bundles/uitotop/js/jquery.ui.totop.min.js'),
+            false,
+            true
+        );
 
         return $template->getResponse();
     }
