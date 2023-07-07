c00e449d881156fdbbf5df03950d293af21cf6b4
Florian Otto <florian.otto@mindbird.de>
2023-07-07 11:26:42 +0200

    Update to contao 5

---
+++ src/DependencyInjection/ContaoUiToTopExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mindbird\ContaoUiToTop\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class ContaoUiToTopExtension extends \Symfony\Component\DependencyInjection\Extension\Extension
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
